88efacc08fb703be6531761c9f37e29bbd09fd91
Andrey Buzon <andrey.bryan.buzon@oocl.com>
2020-10-21 20:42:25 +0800

    [EmployeesAPI][Andrey&Jericah]refactor: remove unused imports

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.thoughtworks.springbootemployee.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employees")
