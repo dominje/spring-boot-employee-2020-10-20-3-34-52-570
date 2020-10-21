7254c51e3fc54eafe7c10201de2a7776f5ec8654
dominje <jericah.domingo@oocl.com>
2020-10-21 17:02:30 +0800

    [EmployeesAPI][Jericah] Feature: Added Employee List to Repository

--- src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeRepository.java
@@ -3,15 +3,19 @@ package com.thoughtworks.springbootemployee.repository;
 import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class EmployeeRepository {
+    private final List<Employee> employeeList = new ArrayList<>();
+
     public List<Employee> findAll() {
-        return null;
+        return employeeList;
     }
 
     public Employee save(Employee employee) {
+        employeeList.add(employee);
         return employee;
     }
 }
