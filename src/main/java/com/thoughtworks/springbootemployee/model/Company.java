822ac3ec568e87c0e2871c7c9492cc23ca24504d
Andrey Buzon <andrey.bryan.buzon@oocl.com>
2020-10-21 21:01:24 +0800

    [EmployeesAPI][Andrey&Jericah]feature: implement attributes to Company model

--- src/main/java/com/thoughtworks/springbootemployee/model/Company.java
+++ src/main/java/com/thoughtworks/springbootemployee/model/Company.java
@@ -1,4 +1,28 @@
 package com.thoughtworks.springbootemployee.model;
 
 public class Company {
+
+    private String companyName;
+    private int companyId;
+
+    public Company(int companyId, String companyName) {
+        this.companyId = companyId;
+        this.companyName = companyName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(int companyId) {
+        this.companyId = companyId;
+    }
 }
