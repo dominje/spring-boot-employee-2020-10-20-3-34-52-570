1e3c891d23fb43c7503399f63619804e3f60f9a2
dominje <jericah.domingo@oocl.com>
2020-10-21 14:22:46 +0800

    [EmployeesAPI][Jericah] Feature: Implement getAllEmployees()

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.thoughtworks.springbootemployee.controller;
+
+import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private final List<Employee> employees = new ArrayList<>();
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employees;
+    }
+
+
+}
