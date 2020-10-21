b8a7b72c7c40eb356aa52e6d28677c16aa235d32
dominje <jericah.domingo@oocl.com>
2020-10-21 16:57:04 +0800

    [EmployeesAPI][Jericah] Refactor: Remove Employee List, added Service in the Controller Constructor

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,14 +14,10 @@ import java.util.stream.Collectors;
 @RequestMapping("/employees")
 public class EmployeeController {
 
-    private final List<Employee> employees = new ArrayList<>();
+    private final EmployeeService service;
 
-    public EmployeeController() {
-        employees.add(new Employee(1,"tom", 21,"Male",8000));
-        employees.add(new Employee(2,"jerry", 22,"Female",7000));
-        employees.add(new Employee(3,"Shrek", 25,"Male",7000));
-        employees.add(new Employee(4,"Fiona", 23,"Female",7000));
-        employees.add(new Employee(5,"Donkey", 14,"Male",7000));
+    public EmployeeController(EmployeeService service) {
+        this.service = service;
     }
 
     @GetMapping
@@ -35,45 +32,45 @@ public class EmployeeController {
         return employee;
     }
 
-    @GetMapping("/{employeeID}")
-    public Employee getEmployeeById(@PathVariable int employeeID) {
-        return employees.stream()
-                .filter(employee ->
-                        employee.getId() == employeeID
-                ).findFirst().orElse(null);
-    }
-
-    @PutMapping("/{employeeID}")
-    public Employee updateEmployeeById(@PathVariable int employeeID, @RequestBody Employee updatedEmployee) {
-        employees.stream().filter(employee ->
-                employee.getId() == employeeID
-        ).findFirst().ifPresent(employee -> {
-                    employees.remove(employee);
-                    employees.add(updatedEmployee);
-                }
-        );
-        return updatedEmployee;
-    }
-
-    @DeleteMapping("/{employeeID}")
-    public void deleteEmployeeById(@PathVariable int employeeID){
-        employees.stream().filter( employee ->
-                employee.getId() == employeeID)
-                .findFirst().ifPresent(employees::remove);
-    }
-
-    @GetMapping(params = "gender")
-    public List<Employee> getEmployeesByGender(@RequestParam String gender){
-        return employees.stream()
-                .filter(employee ->
-                        employee.getGender().equalsIgnoreCase(gender))
-                .collect(Collectors.toList());
-    }
-
-    @GetMapping(params = {"page","pageSize"})
-    public List<Employee> getEmployees(@RequestParam("page") int page, @RequestParam("pageSize") int pageSize){
-        return employees.stream().sorted(Comparator.comparing(Employee::getId)).skip(page).limit(pageSize).collect(Collectors.toList());
-    }
-
+//    @GetMapping("/{employeeID}")
+//    public Employee getEmployeeById(@PathVariable int employeeID) {
+//        return employees.stream()
+//                .filter(employee ->
+//                        employee.getId() == employeeID
+//                ).findFirst().orElse(null);
+//    }
+//
+//    @PutMapping("/{employeeID}")
+//    public Employee updateEmployeeById(@PathVariable int employeeID, @RequestBody Employee updatedEmployee) {
+//        employees.stream().filter(employee ->
+//                employee.getId() == employeeID
+//        ).findFirst().ifPresent(employee -> {
+//                    employees.remove(employee);
+//                    employees.add(updatedEmployee);
+//                }
+//        );
+//        return updatedEmployee;
+//    }
+//
+//    @DeleteMapping("/{employeeID}")
+//    public void deleteEmployeeById(@PathVariable int employeeID){
+//        employees.stream().filter( employee ->
+//                employee.getId() == employeeID)
+//                .findFirst().ifPresent(employees::remove);
+//    }
+//
+//    @GetMapping(params = "gender")
+//    public List<Employee> getEmployeesByGender(@RequestParam String gender){
+//        return employees.stream()
+//                .filter(employee ->
+//                        employee.getGender().equalsIgnoreCase(gender))
+//                .collect(Collectors.toList());
+//    }
+//
+//    @GetMapping(params = {"page","pageSize"})
+//    public List<Employee> getEmployees(@RequestParam("page") int page, @RequestParam("pageSize") int pageSize){
+//        return employees.stream().sorted(Comparator.comparing(Employee::getId)).skip(page).limit(pageSize).collect(Collectors.toList());
+//    }
+//
 
 }
