ffccdbca8698982e734451c5093840483ebea872
dominje <jericah.domingo@oocl.com>
2020-10-22 15:13:14 +0800

    [CompanyAPI][Erik&Jericah] Refactor: Changed file name to Legacy

---
+++ src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepositoryLegacy.java
@@ -0,0 +1,69 @@
+package com.thoughtworks.springbootemployee.repository;
+
+import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+public class CompanyRepositoryLegacy {
+    private final List<Company> companyList = new ArrayList<>();
+    EmployeeRepositoryLegacy employeeRepositoryLegacy = new EmployeeRepositoryLegacy();
+
+    public CompanyRepositoryLegacy() {
+        companyList.add(new Company(1, "Toyota"));
+        companyList.add(new Company(2, "Mitsubishi"));
+        companyList.add(new Company(3, "Ferrari"));
+        companyList.add(new Company(4, "Apple"));
+        companyList.add(new Company(5, "Samsung"));
+    }
+
+    public List<Company> findAll() {
+        return companyList;
+    }
+
+    public Company save(Company companyRequest) {
+        companyList.add(companyRequest);
+        return companyRequest;
+    }
+
+    public Company findCompanyById(int companyId) {
+        return companyList.stream()
+                .filter(company -> company.getCompanyId() == companyId)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Company updateByCompanyId(int companyId, Company updatedCompany) {
+        companyList.stream()
+                .filter(company -> company.getCompanyId()==companyId)
+                .findFirst()
+                .ifPresent(company -> {
+                    companyList.remove(company);
+                    companyList.add(updatedCompany);
+                });
+        return updatedCompany;
+    }
+
+    public List<Employee> getEmployeesByCompanyId(int companyId) {
+        return employeeRepositoryLegacy.findAll().stream()
+                .filter(employee -> employee.getCompany_id()==companyId)
+                .collect(Collectors.toList());
+    }
+
+    public List<Company> getCompanyWithPagination(int page, int pageSize) {
+        return companyList.stream()
+                .sorted(Comparator.comparing(Company::getCompanyId))
+                .skip(page)
+                .limit(pageSize)
+                .collect(Collectors.toList());
+    }
+
+    public void deleteEmployeesByCompanyId(int companyId) {
+        getEmployeesByCompanyId(companyId).forEach(employee -> employeeRepositoryLegacy.deleteById(employee.getId()));
+    }
+}
---
+++ src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeRepositoryLegacy.java
@@ -0,0 +1,69 @@
+package com.thoughtworks.springbootemployee.repository;
+
+import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+public class EmployeeRepositoryLegacy {
+    private List<Employee> employeeList = new ArrayList<>();
+
+    public EmployeeRepositoryLegacy() {
+        employeeList.add(new Employee(1, "Tom", 18, "Male", 1000, 1));
+        employeeList.add(new Employee(2, "Jerry", 18, "Male", 1000, 1));
+        employeeList.add(new Employee(3, "Pooh", 18, "Male", 1000, 3));
+        employeeList.add(new Employee(4, "JJ", 18, "Male", 1000, 2));
+        employeeList.add(new Employee(5, "Yoyo", 18, "Female", 1000, 2));
+    }
+
+    public List<Employee> findAll() {
+        return employeeList;
+    }
+
+    public Employee save(Employee employee) {
+        employeeList.add(employee);
+        return employee;
+    }
+
+    public Employee findById(int employeeId) {
+        return employeeList.stream().filter(employee ->
+                employee.getId() == employeeId)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Employee updateById(int employeeID, Employee updatedEmployee) {
+        employeeList.stream().filter(employee ->
+                employee.getId() == employeeID
+        ).findFirst().ifPresent(employee -> {
+                    employeeList.remove(employee);
+                    employeeList.add(updatedEmployee);
+                }
+        );
+        return updatedEmployee;
+    }
+
+    public void deleteById(int employeeID) {
+        employeeList.stream().filter(employee ->
+                employee.getId() == employeeID)
+                .findFirst().ifPresent(employeeList::remove);
+    }
+
+    public List<Employee> findByGender(String gender) {
+        return employeeList.stream()
+                .filter(employee ->
+                        employee.getGender().equalsIgnoreCase(gender))
+                .collect(Collectors.toList());
+    }
+
+    public List<Employee> getEmployeesWithPagination(int page, int pageSize) {
+        return employeeList.stream().sorted(Comparator.comparing(Employee::getId))
+                .skip(page)
+                .limit(pageSize)
+                .collect(Collectors.toList());
+    }
+}
