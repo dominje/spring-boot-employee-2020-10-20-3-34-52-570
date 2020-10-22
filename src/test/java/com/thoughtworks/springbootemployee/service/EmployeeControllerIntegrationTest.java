63085305f82ebf1bfd6a36a3ae2ec704be2a1e13
dominje <jericah.domingo@oocl.com>
2020-10-22 19:44:26 +0800

    [Employee&CompanyAPI][Erik&Jericah] Test: Initial Integration Test for Employee API

---
+++ src/test/java/com/thoughtworks/springbootemployee/service/EmployeeControllerIntegrationTest.java
@@ -0,0 +1,51 @@
+package com.thoughtworks.springbootemployee.service;
+
+import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.result.RequestResultMatchers;
+
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EmployeeControllerIntegrationTest {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void should_get_all_employees_when_get() throws Exception {
+
+        //given
+        Employee employee = new Employee(1, "Tom", 18, "Male", 1000, 1);
+        employeeRepository.save(employee);
+
+        //when
+        mockMvc.perform(MockMvcRequestBuilders.get("/employees"))
+                //then
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").isNumber())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("Tom"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].age").value(18))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].gender").value("Male"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].salary").value(1000))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].company_id").value(1));
+
+
+    }
+
+}
