a07b6d2e345f9f221c8e1ad8fc18f2f6c602b32f
dominje <jericah.domingo@oocl.com>
2020-10-22 15:13:55 +0800

    [CompanyAPI][Erik&Jericah] Feature: Implement JPA for Company Repository

--- src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
@@ -2,6 +2,7 @@ package com.thoughtworks.springbootemployee.repository;
 
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -10,60 +11,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Repository
-public class CompanyRepository {
-    private final List<Company> companyList = new ArrayList<>();
-    EmployeeRepository employeeRepository = new EmployeeRepository();
+public interface CompanyRepository extends JpaRepository<Company, Integer> {
 
-    public CompanyRepository() {
-        companyList.add(new Company(1, "Toyota"));
-        companyList.add(new Company(2, "Mitsubishi"));
-        companyList.add(new Company(3, "Ferrari"));
-        companyList.add(new Company(4, "Apple"));
-        companyList.add(new Company(5, "Samsung"));
-    }
-
-    public List<Company> findAll() {
-        return companyList;
-    }
-
-    public Company save(Company companyRequest) {
-        companyList.add(companyRequest);
-        return companyRequest;
-    }
-
-    public Company findCompanyById(int companyId) {
-        return companyList.stream()
-                .filter(company -> company.getCompanyId() == companyId)
-                .findFirst()
-                .orElse(null);
-    }
-
-    public Company updateByCompanyId(int companyId, Company updatedCompany) {
-        companyList.stream()
-                .filter(company -> company.getCompanyId()==companyId)
-                .findFirst()
-                .ifPresent(company -> {
-                    companyList.remove(company);
-                    companyList.add(updatedCompany);
-                });
-        return updatedCompany;
-    }
-
-    public List<Employee> getEmployeesByCompanyId(int companyId) {
-        return employeeRepository.findAll().stream()
-                .filter(employee -> employee.getCompanyId()==companyId)
-                .collect(Collectors.toList());
-    }
-
-    public List<Company> getCompanyWithPagination(int page, int pageSize) {
-        return companyList.stream()
-                .sorted(Comparator.comparing(Company::getCompanyId))
-                .skip(page)
-                .limit(pageSize)
-                .collect(Collectors.toList());
-    }
-
-    public void deleteEmployeesByCompanyId(int companyId) {
-        getEmployeesByCompanyId(companyId).forEach(employee -> employeeRepository.deleteById(employee.getId()));
-    }
 }
